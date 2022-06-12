0d9b6a1b1cd643d650f363842aa0d9cd35e309e3
takaya-47 <local.s.s.47@gmail.com>
2022-06-13 08:23:48 +0900

    weatherテーブル作成のためのマイグレファイル作成

--- database/migrations/2022_06_12_023800_create_weather_table.php
+++ database/migrations/2022_06_12_023800_create_weather_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('weather', function (Blueprint $table) {
             $table->id();
+            $table->date('date')->comment('日付');
+            $table->tinyText('prefecture')->comment('都道府県');
+            $table->tinyText('municipalities')->comment('市町村');
+            $table->tinyText('weather')->comment('天気');
+            $table->tinyInteger('highest_temperature')->comment('最高気温');
+            $table->tinyInteger('lowest_temperature')->comment('最低気温');
+            $table->tinyInteger('humidity')->unsigned()->comment('湿度');
+            $table->tinyInteger('rainy_percent')->unsigned()->comment('降水確率');
             $table->timestamps();
         });
     }

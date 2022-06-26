16d7eb94cd0259c58d9d6034f15e1aa38a1b5df5
takaya-47 <local.s.s.47@gmail.com>
2022-06-27 08:56:09 +0900

    天気情報のDB登録を仮実装

--- app/Models/Weather.php
+++ app/Models/Weather.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use \Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\DB;
 
 class Weather extends Model
 {
@@ -16,7 +18,7 @@ class Weather extends Model
     const GEOCODING_API = 'http://api.openweathermap.org/geo/1.0/direct';
     const WEATHER_API   = 'http://api.openweathermap.org/data/2.5/forecast';
 
-    // モデルに関連付けるテーブル
+    // モデルに対応するテーブル名を設定（Laravelのデフォルトはクラスの複数形のスネークケース）
     protected $table = 'weather';
 
     /**
@@ -46,7 +48,7 @@ class Weather extends Model
         } else {
             // 成功以外の場合はログを出した上でエラーコードとメッセージを返却
             $this->make_error_log($response_json);
-            return $this->response_when_error($response_json);
+            return $this->response_when_api_error($response_json);
         }
     }
 
@@ -82,11 +84,46 @@ class Weather extends Model
             // ステータスコードが200以上300未満の時。
             $response = json_decode($response_json->body());
             // 天気予報情報を丸ごと取得して返却。加工は別メソッドで行う。
-            return $response->list;
+            return [$response->list, $response->city];
         } else {
             // 成功以外の場合はログを出した上でエラーコードとメッセージを返却
             $this->make_error_log($response_json);
-            return $this->response_when_error($response_json);
+            return $this->response_when_api_error($response_json);
+        }
+    }
+
+    /**
+     * 天気予報取得APIのレスポンスデータをDBに登録します
+     *
+     * @return int 登録したレコードのID
+     */
+    public function store_response_data_from_weather_api()
+    {
+        list($weather_data_array, $city_data) = $this->fetch_weather_forecast();
+        // 取得データにエラーコードが含まれていたらログ出力して処理を中断する
+        if ( ! empty($weather_data_array['error_code'])) {
+            Log::error('取得データにエラーがあるためDB登録を中断しました', [__METHOD__, 'LINE:' . __LINE__, $weather_data_array]);
+            return;
+        }
+
+        // DB登録処理
+        foreach ($weather_data_array as $key => $weather_data) {
+            try {
+                DB::transaction();
+                $weather = new Weather();
+                $weather->date = $weather_data->dt_txt; // 日時
+                $weather->municipalities = $city_data->name; // 市町村
+                $weather->weather = $weather_data->weather->description; // 天気
+                $weather->highest_temperature = $weather_data->main->temp_max; // 最高気温
+                $weather->lowest_temperature = $weather_data->main->temp_min; // 最低気温
+                $weather->humidity = $weather_data->main->humidity; // 湿度
+                $weather->humidity = $weather_data->pop * 100; // 降水確率(単位：%とするため100を乗じている)
+                $weather->save();
+                DB::commit();
+            } catch (Exception $e) {
+                DB::rollBack();
+                Log::error('DB登録中に予期せぬエラーが発生しました', [__METHOD__, 'LINE:' . __LINE__]);
+            }
         }
     }
 
@@ -100,10 +137,10 @@ class Weather extends Model
     {
         if ($response_json->clientError()) {
             // 400レベルのステータスコード
-            Log::error('[ジオコーディングAPIのエラー]クライアント側でエラーが発生しました', [__METHOD__, 'Line:' . __LINE__, json_decode($response_json->body())]);
+            Log::error('[ジオコーディングAPIのエラー]クライアント側でエラーが発生しました', [__METHOD__, 'LINE:' . __LINE__, json_decode($response_json->body())]);
         } elseif ($response_json->serverError()) {
             // 500レベルのステータスコード
-            Log::error('[ジオコーディングAPIのエラー]サーバー側でエラーが発生しました', [__METHOD__, 'Line:' . __LINE__, json_decode($response_json->body())]);
+            Log::error('[ジオコーディングAPIのエラー]サーバー側でエラーが発生しました', [__METHOD__, 'LINE:' . __LINE__, json_decode($response_json->body())]);
         }
     }
 
@@ -113,7 +150,7 @@ class Weather extends Model
      * @param  Response $response_json
      * @return array
      */
-    public function response_when_error(Response $response_json): array
+    public function response_when_api_error(Response $response_json): array
     {
         $response = json_decode($response_json->body());
         return [

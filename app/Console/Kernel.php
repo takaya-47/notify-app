044cca76341330680acd7aafbbf6930e16fae224
takaya-47 <local.s.s.47@gmail.com>
2022-07-22 08:57:27 +0900

    ローカル環境で毎日5:55に実行されるよう設定

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(NoticeLine::class);
+        $schedule->command(NoticeLine::class)->dailyAt('5:55');
     }
 
     /**
